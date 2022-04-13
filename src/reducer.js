adc723e307d644ee18c3b0284c76798d1541da7a
Evan Fredericks <evan.fredericks@gmail.com>
2022-04-13 14:39:12 -0500

    solution part 6

--- src/reducer.js
+++ src/reducer.js
@@ -1,23 +1,51 @@
-const initialState = {
-  good: 0,
-  ok: 0,
-  bad: 0
-}
+import React from 'react';
+import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import reducer from './reducer'
 
-const counterReducer = (state = initialState, action) => {
-  console.log(action)
-  switch (action.type) {
-    case 'GOOD':
-      return state
-    case 'OK':
-      return state
-    case 'BAD':
-      return state
-    case 'ZERO':
-      return state
-    default: return state
+const store = createStore(reducer)
+
+const App = () => {
+  const good = () => {
+    store.dispatch({
+      type: 'GOOD'
+    })
+  }
+
+  const neutral = () => {
+    store.dispatch({
+      type: 'OK'
+    })
+  }
+
+  const bad = () => {
+    store.dispatch({
+      type: 'BAD'
+    })
+  }
+  const reset = () => {
+    store.dispatch({
+      type: 'ZERO'
+    })
   }
   
+
+  return (
+    <div>
+      <button onClick={good}>good</button> 
+      <button onClick={neutral}>neutral</button> 
+      <button onClick={bad}>bad</button>
+      <button onClick={reset}>reset stats</button>
+      <div>good {store.getState().good}</div>
+      <div>neutral {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
+    </div>
+  )
 }
 
-export default counterReducer
+const renderApp = () => {
+  ReactDOM.render(<App />, document.getElementById('root'))
+}
+
+renderApp()
+store.subscribe(renderApp)
